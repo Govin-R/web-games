ae4c48d138e33dc06827b37f72d062a36a497349
Govin_R <rgovidaraj6092@gmail.com>
2022-06-28 15:19:35 +0530

    ttt previous click need fix

--- TTT/main.js
+++ TTT/main.js
@@ -26,28 +26,66 @@ function startGame(){
 	
 };
 
-
-
+const color = ["openentClick","userClick"];
+const score = [openentScore,yourScore];
+const winPossiblities = [["0",'1','2'],['0','4','8'],['0','3','6'],['1','4','7'],['2','5','8'],['3','4','5'],['6','7','8'],['2','4','6']];
+let yClicked = Array();
+let oClicked = Array();
+let clicked = [];
 function play(a){
+  if((yClicked.includes(a.id) ===false) &&( oClicked.includes(a.id)===false)){
+		if(player===0){
+			a.setAttribute('value',-1);
+			a.classList.add("openentClick");
+			oClicked.push(a.id);
+			clicked.push(a.id);
+
+		//  console.log(a.classList)
+		}
+		else if(player===1){
+			a.setAttribute('value',1);
+			a.classList.add("userClick");
+			yClicked.push(a.id);
+			clicked.push(a.id);
+  	}
   
-  if(player===0){
-    a.setAttribute('value',-1);
-  }else if(player===1){
-    a.setAttribute('value',1);
-  }
-  
-  
-  let currentVal =  [[],[],[]];
-  for(let i = 0; i<3;i++){
-    [r1,r2,r3][i].forEach((e2)=> {
-      //console.log(e2,i)
-      currentVal[i].push(e2.getAttribute('value'));
-    })
-  };
-  console.table(currentVal)
+  /*
+ 	 	let currentVal =  [[],[],[]];
+  	for(let i = 0; i<3;i++){
+    	[r1,r2,r3][i].forEach((e2)=> {
+      	//console.log(e2,i)
+      	currentVal[i].push(e2.getAttribute('value'));
+    	})
+ 	 };
+		//console.table(currentVal);
+		*/
+		//get mark analyze
+		
+		for(let i = 0; i<winPossiblities.length; i++){
+			let tempPosible = winPossiblities[i];
+			let user=[oClicked,yClicked][player];
+			let tempCheck = [];
+			for(let j = 0; j<tempPosible.length; j++){
+				if(user.includes(tempPosible[j])){
+					tempCheck.push(true);
+				}
+			}
+			console.log(tempCheck);
+			if(tempCheck.length===3){
+					score[player].innerText=Number(score[player].innerText)+1;
+			}
+		}
+
+		if(player===1){
+			player=0;
+		}else{player=1;};
+
 	
-	
-	
+
+
+	}
+	else{console.log("clicked: ",a.id in yClicked,a.id in oClicked,"\n","y: ",yClicked,"\n","O: ",oClicked)};
+
 }
 
 
