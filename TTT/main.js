cc8b539738a72c1a241ab973d5eceb0b93c0146e
rgovindaraj6092@gmail.com <rgovindaraj6092@gmail.com>
2022-06-29 10:36:18 +0530

    two player mode done

--- TTT/main.js
+++ TTT/main.js
@@ -2,31 +2,40 @@
 let openentScore = document.getElementById('opScore');
 let yourScore = document.getElementById('yourScore');
 const boxes = document.querySelectorAll('.boxes');
-const r1 = document.querySelectorAll(".r1");
-const r2 = document.querySelectorAll(".r2");
-const r3 = document.querySelectorAll(".r3");
 
-startGame();
+//defaultly 0
+let yClicked = Array();
+let oClicked = Array();
+let clicked = [];
+let checked = [];
+
+startGame();        //game start
 let player=1;
 
 function startGame(){
 	boxes.forEach((e)=>{
-		e.setAttribute("value",0);
+	  e.setAttribute("value",0);
+		//console.log(e.classList);
+		let cls=e.classList;
+		e.classList.remove(...cls);
+	  //console.log(e.classList);
+		e.classList.add('boxes');
 		e.addEventListener("click",()=>{
 		 // e.setAttribute('value',-1)
+	
 		  play(e); })
 	});
 	openentScore.innerText=0;
 	yourScore.innerText=0;
-	
+	[yClicked,oClicked,clicked,checked].forEach(e=> {e.length=0});
 };
 
-const color = ["openentClick","userClick"];
-const score = [openentScore,yourScore];
-let winPossiblities = [["0",'1','2'],['0','4','8'],['0','3','6'],['1','4','7'],['2','5','8'],['3','4','5'],['6','7','8'],['2','4','6']];
-let yClicked = Array();
-let oClicked = Array();
-let clicked = [];
+//some defaults
+const color = ["openentClick", "userClick"];
+const score = [openentScore, yourScore];
+let winPossiblities = [["0", '1', '2'], ['0', '4', '8'], ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8'], ['3', '4', '5'], ['6', '7', '8'], ['2', '4', '6']];
+  
+// after click
 function play(a){
   if((yClicked.includes(a.id) ===false) &&( oClicked.includes(a.id)===false)){
 		if(player===0){
@@ -46,18 +55,25 @@ function play(a){
   	
 		//get mark analyze	
 		for(let i = 0; i<winPossiblities.length; i++){
-			let tempPosible = winPossiblities[i];
+			let tempPosible=winPossiblities[i];
 			let user=[oClicked,yClicked][player];
 			let tempCheck = [];
 			for(let j = 0; j<tempPosible.length; j++){
 				if(user.includes(tempPosible[j])){
-					tempCheck.push(true);
+					tempCheck.push(tempPosible[j]);
 				}
 			}
-			if(tempCheck.length===3){
-				console.log(tempCheck);
+			
+			let sumtc='';
+			tempCheck.forEach(x=> {
+			  sumtc+=x;
+			});
+			if(tempCheck.length===3 && checked.includes(sumtc)==false){
+        //console.log(tempCheck);
+        checked.push(sumtc);
+        //console.log(checked);
 				winPossiblities.splice(winPossiblities.indexOf(tempCheck),1);
-				console.log(winPossiblities)
+				
 					score[player].innerText=Number(score[player].innerText)+1;
 			}
 		}
@@ -65,11 +81,13 @@ function play(a){
 		if(player===1){
 			player=0;
 		}else{player=1;};
-
-
-
+		
 	}
-	else{console.log("clicked: ",a.id in yClicked,a.id in oClicked,"\n","y: ",yClicked,"\n","O: ",oClicked)};
+	else{//console.log("clicked: ",a.id in yClicked,a.id in oClicked,"\n","y: ",yClicked,"\n","O: ",oClicked)
+	};
+	
+	if(clicked.length===9){
+	  setTimeout(()=> {startGame()},2000)};
 
 }
 
